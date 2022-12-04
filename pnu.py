206b7c2381e3e5ebc0415df3db1a2fc8b0300f7d
lemonbuilder <103490406+lemonbuilder@users.noreply.github.com>
2022-12-04 19:27:43 +0900

    Update pnu.py

--- pnu.py
+++ pnu.py
@@ -6,7 +6,8 @@ from google.cloud import storage
 
 st.subheader("PNU Making Machine")
 st.text('PNU(필지고유번호)는 공간데이터 상에서 필지마다 고유하게 부여된 번호로, 필지별 정보확인에 사용합니다.')
-st.info('지번주소를 입력하고 하단의 [PNU로 변환] 버튼을 클릭하세요.')
+st.text('지번주소를 입력하고 하단의 [PNU로 변환] 버튼을 클릭하세요.')
+st.markdown('---')
 
 a = st.text_input("시/도: ")
 b = st.text_input("시/군/구: ")
