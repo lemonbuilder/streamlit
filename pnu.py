30d0af28f4c8aa51f36606b406edd55b6dd29d51
lemonbuilder <103490406+lemonbuilder@users.noreply.github.com>
2022-12-04 19:22:53 +0900

    Update pnu.py

--- pnu.py
+++ pnu.py
@@ -6,7 +6,7 @@ from google.cloud import storage
 
 st.subheader("PNU Making Machine")
 
-st.info('PNU(필지고유번호)는 공간데이터 상에서 필지마다 고유하게 부여된 번호로, 필지정보를 확인할 때 사용합니다.')
+st.info('PNU(필지고유번호)는 공간데이터 상에서 필지마다 고유하게 부여된 번호로, 필지별 정보확인에 사용합니다.')
 
 a = st.text_input("시/도: ")
 b = st.text_input("시/군/구: ")
