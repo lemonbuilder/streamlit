7065aa1510af629ec6a32a2e1d9f7db3caa5da3d
lemonbuilder <103490406+lemonbuilder@users.noreply.github.com>
2022-12-04 18:11:28 +0900

    Update pnu.py

--- pnu.py
+++ pnu.py
@@ -49,7 +49,7 @@ for q in range(len(data)):
                     if d in data[q]:
                         num = data[q][0]
                         
-if num == None:
+if num == '' or num == None:
     num = '{:010d}'.format(0)
                         
 if e == '' or e == None:
